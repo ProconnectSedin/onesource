200083d4139db8db9978538eb16a6cbca6e0f3df
Suresh <suresh@sedintechnologies.com>
2022-12-24 19:09:21 +0530

    Testing _ Jenkins

--- Procedures/dwh/usp_d_address.sql
+++ Procedures/dwh/usp_d_address.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE dwh.usp_d_address(IN p_sourceid character varying, IN p_dataflowflag character varying, IN p_targetobject character varying, OUT srccnt integer, OUT inscnt integer, OUT updcnt integer, OUT dltcount integer, INOUT flag1 character varying, OUT flag2 character varying)
+CREATE or REPLACE PROCEDURE dwh.usp_d_address(IN p_sourceid character varying, IN p_dataflowflag character varying, IN p_targetobject character varying, OUT srccnt integer, OUT inscnt integer, OUT updcnt integer, OUT dltcount integer, INOUT flag1 character varying, OUT flag2 character varying)
     LANGUAGE plpgsql
     AS $$
 DECLARE 
