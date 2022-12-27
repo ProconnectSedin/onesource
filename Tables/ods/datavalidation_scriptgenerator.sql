26701acf112eebfaa353e8ee288d6cfe465c07d4
Selva <selva@sedintechnologies.com>
2022-12-27 15:03:09 +0530

    data validation scripts

--- Tables/ods/datavalidation_scriptgenerator.sql
+++ Tables/ods/datavalidation_scriptgenerator.sql
@@ -1,4 +1,9 @@
-CREATE TABLE ods.datavalidation_scriptgenerator (
+-- Table: ods.datavalidation_scriptgenerator
+
+ DROP TABLE IF EXISTS ods.datavalidation_scriptgenerator;
+
+CREATE TABLE IF NOT EXISTS ods.datavalidation_scriptgenerator
+(
     row_id bigint NOT NULL,
     db character varying(100) COLLATE public.nocase,
     schemaname character varying(100) COLLATE public.nocase,
@@ -8,5 +13,11 @@ CREATE TABLE ods.datavalidation_scriptgenerator (
     join_tablename text COLLATE public.nocase,
     join_columnname text COLLATE public.nocase,
     whereclause_columnname text COLLATE public.nocase,
-    groupby_columnname text COLLATE public.nocase
-);
+    groupby_columnname text COLLATE public.nocase,
+    CONSTRAINT datavalidation_scriptgenerator_pkey PRIMARY KEY (row_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS ods.datavalidation_scriptgenerator
+    OWNER to proconnect;
