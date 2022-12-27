aef176e135d9d6b38c3739dd824a129d4ddf89ff
Selva <selva@sedintechnologies.com>
2022-12-27 13:01:37 +0530

    stg_datavalidation

--- Tables/stg/stg_datavalidation.sql
+++ Tables/stg/stg_datavalidation.sql
@@ -1,8 +1,18 @@
-CREATE TABLE stg.stg_datavalidation (
-    sourcetable character varying,
-    dimension character varying,
-    period character varying,
+-- Table: stg.stg_datavalidation
+
+DROP TABLE IF EXISTS stg.stg_datavalidation;
+
+CREATE TABLE IF NOT EXISTS stg.stg_datavalidation
+(
+    sourcetable character varying COLLATE pg_catalog."default",
+    dimension character varying COLLATE pg_catalog."default",
+    period character varying COLLATE pg_catalog."default",
     sourcedatacount bigint,
     createddate timestamp without time zone,
     etlcreateddate timestamp without time zone
-);
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS stg.stg_datavalidation
+    OWNER to proconnect;
