3c70f7ca729b429a107b6dc37e08bf870d7c223a
akash <akash@sedintechnologies.com>
2022-12-29 18:18:56 +0530

    raw table staging table

--- Tables/raw/raw_client_service_log.sql
+++ Tables/raw/raw_client_service_log.sql
@@ -1,38 +1,28 @@
-CREATE TABLE raw.raw_client_service_log (
-    raw_id bigint NOT NULL,
+-- Table: raw.raw_client_service_log
+
+-- DROP TABLE IF EXISTS "raw".raw_client_service_log;
+
+CREATE TABLE IF NOT EXISTS "raw".raw_client_service_log
+(
+    raw_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
     cust_id integer NOT NULL,
     client_name character varying(50) COLLATE public.nocase,
     service_name character varying(50) COLLATE public.nocase,
     service_type character varying(50) COLLATE public.nocase,
-    client_json text,
+    client_json text COLLATE pg_catalog."default",
     status character varying(50) COLLATE public.nocase,
-    error_massage text,
+    error_message text COLLATE pg_catalog."default",
     key_search1 character varying(50) COLLATE public.nocase,
     key_search2 character varying(50) COLLATE public.nocase,
     key_search3 character varying(50) COLLATE public.nocase,
     key_search4 character varying(50) COLLATE public.nocase,
     ip_address character varying(20) COLLATE public.nocase,
     created_date timestamp without time zone,
-    etlcreateddatetime timestamp(3) without time zone DEFAULT now()
-);
+    etlcreateddatetime timestamp(3) without time zone DEFAULT now(),
+    CONSTRAINT raw_client_service_log_pkey PRIMARY KEY (raw_id)
+)
 
-ALTER TABLE raw.raw_client_service_log ALTER COLUMN cust_id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME raw.raw_client_service_log_cust_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
+TABLESPACE pg_default;
 
-ALTER TABLE raw.raw_client_service_log ALTER COLUMN raw_id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME raw.raw_client_service_log_raw_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
-
-ALTER TABLE ONLY raw.raw_client_service_log
-    ADD CONSTRAINT raw_client_service_log_pkey PRIMARY KEY (raw_id);
+ALTER TABLE IF EXISTS "raw".raw_client_service_log
+    OWNER to proconnect;
--- Tables/stg/stg_client_service_log.sql
+++ Tables/stg/stg_client_service_log.sql
@@ -1,28 +1,27 @@
-CREATE TABLE stg.stg_client_service_log (
+-- Table: stg.stg_client_service_log
+
+-- DROP TABLE IF EXISTS stg.stg_client_service_log;
+
+CREATE TABLE IF NOT EXISTS stg.stg_client_service_log
+(
     cust_id integer NOT NULL,
     client_name character varying(50) COLLATE public.nocase,
     service_name character varying(50) COLLATE public.nocase,
     service_type character varying(50) COLLATE public.nocase,
-    client_json text,
+    client_json text COLLATE public.nocase NOT NULL,
     status character varying(50) COLLATE public.nocase,
-    error_massage text,
+    error_message text COLLATE public.nocase,
     key_search1 character varying(50) COLLATE public.nocase,
     key_search2 character varying(50) COLLATE public.nocase,
     key_search3 character varying(50) COLLATE public.nocase,
     key_search4 character varying(50) COLLATE public.nocase,
     ip_address character varying(20) COLLATE public.nocase,
     created_date timestamp without time zone,
-    etlcreateddatetime timestamp(3) without time zone DEFAULT now()
-);
+    etlcreateddatetime timestamp(3) without time zone DEFAULT now(),
+    CONSTRAINT pk_client_1 PRIMARY KEY (cust_id)
+)
 
-ALTER TABLE stg.stg_client_service_log ALTER COLUMN cust_id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME stg.stg_client_service_log_cust_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1
-);
+TABLESPACE pg_default;
 
-ALTER TABLE ONLY stg.stg_client_service_log
-    ADD CONSTRAINT pk_client_1 PRIMARY KEY (cust_id);
+ALTER TABLE IF EXISTS stg.stg_client_service_log
+    OWNER to proconnect;
