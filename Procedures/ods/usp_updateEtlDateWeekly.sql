9057e4e705a0d7f17c2a444a35886733b7d5582b
Sivani <sivani@sedintechnologies.com>
2023-01-23 17:39:11 +0530

    added extra tables for refresh

--- Procedures/ods/usp_updateEtlDateWeekly.sql
+++ Procedures/ods/usp_updateEtlDateWeekly.sql
@@ -10,7 +10,7 @@ AS $BODY$
 BEGIN
 
 update  ods.controldetail 
-set  etllastrundate = (CURRENT_DATE - INTERVAL '7 days')::DATE
+set  etllastrundate = (CURRENT_DATE - INTERVAL '15 days')::DATE
 where sourceid in 
 (
 	'wms_outbound_sch_dtl'				, 'sin_item_dtl'				,
@@ -22,7 +22,7 @@ where sourceid in
 	'wms_put_exec_item_dtl'				, 'wms_ex_itm_su_conversion_dtl',
 	'wms_pick_plan_dtl'					, 'wms_bin_exec_item_dtl'		,
 	'wms_outbound_item_detail_h'		, 'wms_bin_exec_item_detail'	,
-	'wms_asn_detail_h'					, 'tms_brsd_shipment_details'	,
+	'tms_brsd_shipment_details'			, 'spy_voucher_hdr'				,
 	'wms_contract_dtl_h'				, 'wms_dispatch_dtl'			,
 	'wms_pack_exec_thu_hdr'				, 'wms_asn_add_dtl'				,
 	'wms_pack_exec_thu_dtl'				, 'wms_gr_thu_dtl'				,
@@ -32,17 +32,32 @@ where sourceid in
 	'snp_voucher_dtl'					, 'wms_inbound_item_detail'		, 		
 	'wms_inbound_sch_item_detail'		, 'wms_pick_exec_dtl'			, 			
 	'wms_inbound_sch_item_detail_h'		, 'wms_int_ord_bin_dtl'			, 			
-	'wms_put_exec_serial_dtl'			, 'wms_contract_hdr_h'			,
-	'wms_inbound_header_h'				, 'sin_invoice_hdr'				,
-	'wms_asn_header_h'					, 'jv_voucher_trn_dtl'			,
+	'wms_put_exec_serial_dtl'			, 'wms_inbound_header_h'		, 
+	'sin_invoice_hdr'					, 'jv_voucher_trn_dtl'			,
 	'snp_fbposting_dtl'					, 'snp_voucher_dtl'				,
 	'snp_voucher_hdr'					, 'wms_pack_plan_dtl'			,
 	'wms_asn_detail'					, 'wms_wave_dtl'				,
 	'wms_inbound_item_detail_h'			, 'spy_paybatch_dtl'			,	
 	'sin_ap_postings_dtl'				, 'wms_put_plan_item_dtl'		,
-	'wms_outbound_header_h'
+    'wms_outbound_header_h'             , 'ci_doc_hdr'  
 ) 
  and dataflowflag = 'SRCtoStg';
+ 
+update  ods.controldetail 
+set  etllastrundate = (CURRENT_DATE - INTERVAL '45 days')::DATE
+where sourceid in 
+(
+	'wms_asn_header_h', 'wms_asn_detail_h','wms_contract_transfer_inv_dtl','cbadj_adjv_crdoc_dtl','sad_adjv_drdoc_dtl','sad_adjv_crdoc_dtl'
+)
+and dataflowflag = 'SRCtoStg';
+ 
+update  ods.controldetail 
+set  etllastrundate = (CURRENT_DATE - INTERVAL '365 days')::DATE
+where sourceid in 
+(
+	'cbadj_adjv_drdoc_dtl', 'wms_contract_hdr_h'
+)
+and dataflowflag = 'SRCtoStg';
  
 END;
 $BODY$;
