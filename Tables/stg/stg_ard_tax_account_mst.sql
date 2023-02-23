ca71769c03e4f12a036fe9f0868cfe995db29453
Sivani <sivani@sedintechnologies.com>
2023-02-23 15:28:13 +0530

    dimension stg tables

--- Tables/stg/stg_ard_tax_account_mst.sql
+++ Tables/stg/stg_ard_tax_account_mst.sql
@@ -1,27 +1,31 @@
--- Table: stg.stg_ard_tax_addn_acct_mst
+-- Table: stg.stg_ard_tax_account_mst
 
--- DROP TABLE IF EXISTS stg.stg_ard_tax_addn_acct_mst;
+-- DROP TABLE IF EXISTS stg.stg_ard_tax_account_mst;
 
-CREATE TABLE IF NOT EXISTS stg.stg_ard_tax_addn_acct_mst
+CREATE TABLE IF NOT EXISTS stg.stg_ard_tax_account_mst
 (
     company_code character varying(40) COLLATE public.nocase NOT NULL,
     tax_type character varying(100) COLLATE public.nocase NOT NULL,
     tax_community character varying(100) COLLATE public.nocase NOT NULL,
-    usage_id character varying(80) COLLATE public.nocase NOT NULL,
+    tax_class character varying(160) COLLATE public.nocase NOT NULL,
+    tax_category character varying(160) COLLATE public.nocase NOT NULL,
     tax_region character varying(40) COLLATE public.nocase NOT NULL,
+    tax_accounttype character varying(160) COLLATE public.nocase NOT NULL,
     effective_from timestamp without time zone NOT NULL,
     sequence_no integer NOT NULL,
     finance_book character varying(80) COLLATE public.nocase,
+    tax_group character varying(40) COLLATE public.nocase NOT NULL,
+    tax_code character varying(40) COLLATE public.nocase NOT NULL,
     account_code character varying(128) COLLATE public.nocase NOT NULL,
     effective_to timestamp without time zone,
     cost_center character varying(40) COLLATE public.nocase,
     analysis_code character varying(20) COLLATE public.nocase,
     subanalysis_code character varying(20) COLLATE public.nocase,
     etlcreateddatetime timestamp(3) without time zone DEFAULT now(),
-    CONSTRAINT ard_tax_addn_acct_mst_pk PRIMARY KEY (company_code, tax_type, tax_community, usage_id, effective_from, sequence_no)
+    CONSTRAINT ard_tax_account_mst_pk PRIMARY KEY (company_code, tax_type, tax_community, tax_class, tax_category, tax_region, tax_accounttype, effective_from, sequence_no, tax_group, tax_code, account_code)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS stg.stg_ard_tax_addn_acct_mst
+ALTER TABLE IF EXISTS stg.stg_ard_tax_account_mst
     OWNER to proconnect;
